9107f4516c814f0c4a783040691e062f75518035
Akami-Slayer <akamislayer553@gmail.com>
2020-11-23 10:34:15 -0500

    Day 2 Excersises

---
+++ Day2/excercise1.py
@@ -0,0 +1,10 @@
+#!/bin/python3
+
+two_digit_number = input("Type a two digit number: ")
+
+
+number_a = int(two_digit_number[0])
+number_b = int(two_digit_number[1])
+
+print (number_a + number_b)
+
