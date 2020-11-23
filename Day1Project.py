06225c291142816723d76b517b64ea90ef710f74
Akami-Slayer <akamislayer553@gmail.com>
2020-11-23 00:41:01 -0500

    Day 1 Project

---
+++ Day1Project.py
@@ -0,0 +1,9 @@
+#!/bin/python3
+
+print ("Hello!, and welcome to the Band Name Generator.\n")
+
+city_name = input ("First lets start with the city you grew up in:\n")
+pet_name = input ("Now, Whats the name of a pet?\n")
+
+print ("The name of yor band is: " + city_name + " " + pet_name)
+
